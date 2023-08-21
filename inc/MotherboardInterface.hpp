457c51bd12a64379552339968d5e871f9d430f54
Vladimir Lekhterev <vovalekhterev@gmail.com>
2023-08-21 16:35:37 +0300

    Move several structs to nested namespace & use pointers instead of vectors

--- inc/MotherboardInterface.hpp
+++ inc/MotherboardInterface.hpp
@@ -37,79 +37,57 @@ public:
   }
 };
 
-struct TTYConfig final {
-  std::string Port;
-  size_t Baudrate;
-
-  enum { STOPBITS_ONE, STOPBITS_TWO } Stopbits;
-
-  bool ParityBit;
-  float Timeout;
-};
-
-struct SOMValues {
-  static constexpr uint8_t SOM1 = 0xFF;
-  static constexpr uint8_t SOM2 = 0xAA;
-  static constexpr uint8_t SOM3 = 0xFF;
-};
-
-struct Periphery {
-  static constexpr uint8_t Body = 0;
-  static constexpr uint8_t Imu = 0;
-};
-
-class OutPackage {
-  uint8_t PeripheryId;
-  uint8_t RequestSize;
-  uint8_t ResponceSize;
-  uint8_t MetaInfo;
-  std::vector<uint8_t> Data;
-
-public:
-  OutPackage(uint8_t peripheryId, uint8_t responceSize, uint8_t metaInfo,
-             std::vector<uint8_t> &&data)
-      : PeripheryId{peripheryId}, RequestSize{data.size()},
-        ResponceSize{responceSize}, MetaInfo{metaInfo}, Data{std::move(data)} {}
-
-  size_t GetSize() const { return Data.size() + 7; }
-
-  void SerializeTo(uint8_t *dest, size_t *size) const {
-    assert(dest);
-    assert(size);
-
-    *size = GetSize();
-
-    uint8_t *ptr = dest;
-
-    *(ptr++) = SOMValues::SOM1;
-    *(ptr++) = SOMValues::SOM2;
-    *(ptr++) = PeripheryId;
-    *(ptr++) = RequestSize;
-    *(ptr++) = ResponceSize;
-    *(ptr++) = MetaInfo;
-
-    memcpy(ptr, Data.data(), Data.size());
-    ptr += Data.size();
-
-    *ptr = SOMValues::SOM3;
-  }
-};
-
-struct InPackage {
-  uint8_t PeripheryId;
-  uint8_t MetaInfo;
-  uint8_t Error;
-  std::vector<uint8_t> Data;
-};
-
 class MBInterface final {
-  bool HasError = false;
-  std::string Error;
+public:
+  struct TTYConfig final {
+    std::string Port;
+    size_t Baudrate;
+
+    enum { STOPBITS_ONE, STOPBITS_TWO } Stopbits;
+
+    bool ParityBit;
+    float Timeout;
+  };
+
+  struct SOMValues {
+    static constexpr uint8_t SOM1 = 0xFF;
+    static constexpr uint8_t SOM2 = 0xAA;
+    static constexpr uint8_t SOM3 = 0xAF;
+  };
+
+  struct Periphery {
+    static constexpr uint8_t Body = 0;
+    static constexpr uint8_t Imu = 0;
+  };
+
+  struct OutPackage {
+    uint8_t PeripheryId;
+    uint8_t ResponceSize;
+    uint8_t MetaInfo;
+
+    const uint8_t *Data;
+    uint8_t RequestSize;
+  };
+
+  struct InPackage {
+    uint8_t PeripheryId;
+    uint8_t MetaInfo;
+    uint8_t Error;
+
+    const uint8_t* Data;
+    uint8_t ResponceSize;
+  };
+
+private:
+  static constexpr uint8_t BufferSize = 512;
 
   DescriptorWrapper Fd;
   TTYConfig TTY;
 
-  std::array<uint8_t, 512> Buffer;
+  bool HasError = false;
+  std::string Error;
+
+  std::array<uint8_t, BufferSize> Buffer;
 
 private:
   bool MakeError(const std::string &msg) {
@@ -123,7 +101,58 @@ private:
                      std::string{strerror(errno)});
   }
 
+  void SerializePackageToBuf(const OutPackage &package, size_t *size) {
+    assert(package.Data);
+    assert(size);
+
+    *size = package.RequestSize + 7;
+    assert(*size < BufferSize);
+
+    uint8_t *ptr = Buffer.data();
+
+    *(ptr++) = SOMValues::SOM1;
+    *(ptr++) = SOMValues::SOM2;
+    *(ptr++) = package.PeripheryId;
+    *(ptr++) = package.RequestSize;
+    *(ptr++) = package.ResponceSize;
+    *(ptr++) = package.MetaInfo;
+
+    memcpy(ptr, package.Data, package.ResponceSize);
+    ptr += package.ResponceSize;
+
+    *ptr = SOMValues::SOM3;
+  }
+
+  bool Read(uint8_t* buf, size_t size) {
+    if (read(Fd.Get(), buf, size) <= 0)
+      return MakeTTYError(TTY, "Failed to read");
+    return true;
+  }
+
+  bool ReadToBuf(size_t size) {
+    return Read(Buffer.data(), size);
+  }
+
+  bool EnsureSom(size_t windowSize) {
+    if (Buffer[0] == SOMValues::SOM1)
+      return true;
+
+    std::cout << "Warning: Package sequence corrupted, attempting to fix..."
+              << std::endl;
+
+    size_t i = 0;
+    while (Buffer[0] != SOMValues::SOM1 && i++ < windowSize) {
+      if (read(Fd.Get(), Buffer.data(), 1) <= 0)
+        return false;
+    }
+
+    return Buffer[0] == SOMValues::SOM1;
+  }
+
 public:
+  bool IsOk() const { return HasError; }
+  std::string GetError() const { return Error; }
+
   MBInterface() = default;
 
   bool Configure(const TTYConfig &ttyConfig) {
@@ -143,7 +172,6 @@ public:
 
     if (ttyConfig.Stopbits == TTYConfig::STOPBITS_ONE)
       tty.c_cflag &= ~CSTOPB;
-
     if (ttyConfig.Stopbits == TTYConfig::STOPBITS_TWO)
       tty.c_cflag |= CSTOPB;
 
@@ -171,10 +199,8 @@ public:
     if (!Fd.IsValid())
       return MakeError("TTY Interface is not set up");
 
-    assert(package.GetSize() <= 512);
-
     size_t size;
-    package.SerializeTo(Buffer.data(), &size);
+    SerializePackageToBuf(package, &size);
 
     if (write(Fd.Get(), Buffer.data(), size) <= 0)
       return MakeTTYError(TTY, "Failed to write");
@@ -182,73 +208,43 @@ public:
     return true;
   }
 
-  bool Read(size_t numBytes) {
-    if (read(Fd.Get(), Buffer.data(), numBytes) <= 0)
-      return MakeTTYError(TTY, "Failed to read");
-    return true;
-  }
-
-  bool EnsureSom(size_t windowSize) {
-    if (Buffer[0] == SOMValues::SOM1)
-      return true;
-
-    std::cout << "Warning: Package sequence corrupted, attempting to fix..."
-              << std::endl;
-
-    size_t i = 0;
-    while (Buffer[0] != SOMValues::SOM1 && i++ < windowSize) {
-      if (read(Fd.Get(), Buffer.data(), 1) <= 0)
-        return false;
-    }
-
-    return Buffer[0] == SOMValues::SOM1;
-  }
-
   bool Receive(size_t responceSize, InPackage &package) {
-    if (read(Fd.Get(), Buffer.data(), 1) <= 0)
-      return MakeTTYError(TTY, "Failed to read");
-
+    if (!ReadToBuf(1))
+      return false;
+    
     if (!EnsureSom(256))
       return MakeError("Port " + TTY.Port + ": Sequence corrupted");
 
-    if (!Read(1))
+    if (!ReadToBuf(1))
       return false;
     if (Buffer[0] != SOMValues::SOM2)
       return MakeTTYError(TTY, "Broken package");
 
-    if (!Read(1))
+    if (!ReadToBuf(1))
       return false;
     package.PeripheryId = Buffer[0];
 
-    if (!Read(1))
+    if (!ReadToBuf(1))
       return false;
     package.MetaInfo = Buffer[0];
 
-    if (!Read(1))
+    if (!ReadToBuf(1))
       return false;
     package.Error = Buffer[0];
 
-    if (!Read(responceSize))
+    if (!ReadToBuf(responceSize))
       return false;
-    
-    package.Data.resize(responceSize);
-    std::copy(Buffer.begin(), Buffer.begin() + responceSize,
-              package.Data.begin());
 
-    if (!Read(1)) return false;
+    package.Data = Buffer.data();
 
-    if (Buffer[0] != SOMValues::SOM3)
+    uint8_t som;
+    if (!Read(&som, 1))
+      return false;
+
+    if (som != SOMValues::SOM3)
       return MakeTTYError(TTY, "Unexpected end of package");
 
     return true;
-  }
-
-  bool IsOk() const {
-    return HasError;
-  }
-
-  std::string GetError() const {
-    return Error;
   }
 };
 
