ba98a38f838d41e75fcaa61fb4aabc437480083a
spanovs01 <spanovs01frag@egmail.com>
2023-08-23 21:10:38 +0300

    Create IMU interface facade

--- inc/IMUInterface.hpp
+++ inc/IMUInterface.hpp
@@ -1,47 +1,15 @@
 #pragma once
 
-#include "MBInterface.hpp"
+#include "IMURPC.hpp"
 
-namespace Roki {
-class IMUInterface {
-public:
-  struct TimestampT {
-    size_t TimeS;
-    size_t TimeNS;
-  };
-
-  struct Quaternion {
-    float X;
-    float Y;
-    float Z;
-    float W;
-
-    //float Accuracy;
-  };
-
-  struct IMUFrame {
-    Quaternion Orientation;
-    TimestampT Timestamp;
-
-    uint8_t SensorID;
-  };
-
-private:
-  static constexpr uint8_t ResponceSize = 4 * sizeof(int16_t) + /* sizeof(float) + */
-                                          2 * sizeof(uint32_t) +
-                                          sizeof(uint8_t);
-
-  static constexpr uint8_t RequestSize = 2;
-
-  bool HasError = false;
-  std::string Error;
-
-  IMUFrame Deserialize(const MBInterface::InPackage &package);
-
-public:
-  bool IsOk() const;
-  std::string GetError() const;
-  bool GetFrame(MBInterface &mbi, uint16_t seq, IMUFrame &dest);
-};
-
-} // namespace Roki
+namespace Roki
+{
+    class IMUInterface : public IMURPCStub
+    {
+    public:
+        bool GetFrame(MBInterface &mbi, uint16_t seq, IMUFrame &frame);
+        bool GetInfo(MBInterface &mbi, IMUInfo &info);
+        bool GetLastFrame(MBInterface &mbi, IMUFrame &frame);
+        bool ResetCounter(MBInterface &mbi);
+    };
+}
--- src/IMUInterface.cpp
+++ src/IMUInterface.cpp
@@ -1,79 +1,28 @@
 #include "IMUInterface.hpp"
-namespace Roki {
+namespace Roki
+{
+    bool IMUInterface::GetFrame(MBInterface &mbi, uint16_t seq, IMUFrame &frame)
+    {
+        IMUFrameRequest request;
+        request.seq = seq;
+        return PerformRPC(mbi, request, frame);
+    }
 
-IMUInterface::IMUFrame
-IMUInterface::Deserialize(const MBInterface::InPackage &package) {
-  assert(package.ResponceSize == ResponceSize);
+    bool IMUInterface::GetInfo(MBInterface &mbi, IMUInfo &info) {
+        IMUInfoRequest request = {};
+        return PerformRPC(mbi, request, info);
+    }
 
-  IMUFrame fr;
-  // cppcheck-suppress uninitStructMember
-  auto &qt = fr.Orientation;
+    bool IMUInterface::GetLastFrame(MBInterface &mbi, IMUFrame &frame)
+    {
+        IMULatestRequest request = {};
+        return PerformRPC(mbi, request, frame);
+    }
 
-  // cppcheck-suppress uninitStructMember
-  auto &ts = fr.Timestamp;
-
-  const uint8_t *ptr = package.Data;
-  const float norm = 16384; // 2**14 
-
-  qt.X = float(*reinterpret_cast<const int16_t *>(ptr)) / norm;
-  ptr += sizeof(int16_t);
-
-  qt.Y = float(*reinterpret_cast<const int16_t *>(ptr)) / norm;
-  ptr += sizeof(int16_t);
-
-  qt.Z = float(*reinterpret_cast<const int16_t *>(ptr)) / norm;
-  ptr += sizeof(int16_t);
-
-  qt.W = float(*reinterpret_cast<const int16_t *>(ptr)) / norm;
-  ptr += sizeof(int16_t);
-  
-  /*
-  qt.Accuracy = *reinterpret_cast<const float *>(ptr);
-  ptr += sizeof(float);
-  */
-
-  ts.TimeS = *reinterpret_cast<const uint32_t *>(ptr);
-  ptr += sizeof(uint32_t);
-
-  ts.TimeNS = *reinterpret_cast<const uint32_t *>(ptr);
-  ptr += sizeof(uint32_t);
-
-  fr.SensorID = *reinterpret_cast<const uint8_t *>(ptr);
-  ptr += sizeof(uint8_t);
-
-  return fr;
+    bool IMUInterface::ResetCounter(MBInterface &mbi)
+    {
+        IMUResetRequest request = {};
+        Empty responce = {};
+        return PerformRPC(mbi, request, responce);
+    }
 }
-
-bool IMUInterface::IsOk() const { return !HasError; }
-std::string IMUInterface::GetError() const { return Error; }
-
-bool IMUInterface::GetFrame(MBInterface &mbi, uint16_t seq, IMUFrame &dest) {
-  std::array<uint8_t, 2> buf{0, 0};
-  *reinterpret_cast<uint16_t *>(buf.data()) = seq;
-
-  MBInterface::OutPackage outPackage{MBInterface::Periphery::Imu, ResponceSize,
-                                     0, buf.data(), buf.size()};
-
-  if (!mbi.Send(outPackage)) {
-    HasError = true;
-    Error = mbi.GetError();
-    return false;
-  }
-
-  MBInterface::InPackage inPackage;
-  if (!mbi.Receive(ResponceSize, inPackage)) {
-    HasError = true;
-    Error = mbi.GetError();
-    return false;
-  }
-
-  if (inPackage.Error != 0) {
-    HasError = true;
-    Error = "Frame " + std::to_string(seq) + " is unavailable";
-    return false;
-  }
-
-  dest = Deserialize(inPackage);
-  return true;
-}
-} // namespace Roki
