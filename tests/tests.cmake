3b0033d78a58359d70f8078b3b47d1b7851899a1
spanovs01 <spanovs01frag@egmail.com>
2023-08-27 19:41:31 +0300

    Update CMakeLists.txt

--- tests/tests.cmake
+++ tests/tests.cmake
@@ -1,2 +1,5 @@
 add_executable(TestIMU tests/TestIMU.cpp)
 target_link_libraries(TestIMU PRIVATE Motherboard pthread)
+
+add_executable(TestBody tests/TestBody.cpp)
+target_link_libraries(TestBody PRIVATE Motherboard Rcb4Adapter pthread)
