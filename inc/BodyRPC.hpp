dccaed4328b5b2f4652d1004591bd63da894a375
spanovs01 <spanovs01frag@egmail.com>
2023-08-27 19:39:32 +0300

    Continue work on body interface

--- inc/BodyRPC.hpp
+++ inc/BodyRPC.hpp
@@ -2,42 +2,63 @@
 
 #include "SerialInterface.hpp"
 
-namespace Roki {
-class BodyRPC {
-public:
-  struct MessageMode {
-    using Type = uint8_t;
+namespace Roki
+{
+  class BodyRPC
+  {
+  public:
+    struct MessageMode
+    {
+      using Type = uint8_t;
 
-    static constexpr Type Sync = 0;
-    static constexpr Type Async = 1;
+      static constexpr Type Sync = 0;
+      static constexpr Type Async = 1;
 
-    static uint8_t Serialize(MessageMode::Type mode);
-    static MessageMode::Type Deserialize(uint8_t data);
+      static uint8_t Serialize(MessageMode::Type mode);
+      static Type Deserialize(uint8_t data);
+    };
+
+    struct ErrorCode final
+    {
+      using Type = uint8_t;
+      static constexpr Type Success = 0;
+      static constexpr Type Timeout = 1;
+      static constexpr Type Unknown = 2;
+
+      static uint8_t Serialize(Type mode);
+      static Type Deserialize(uint8_t val);
+
+      static std::string GetDescription(Type error);
+    };
+
+    struct Request
+    {
+      const uint8_t *Data;
+      uint8_t RequestSize;
+      uint8_t ResponceSize;
+      MessageMode::Type Mode;
+    };
+
+    struct Responce
+    {
+      const uint8_t *Data;
+      uint8_t ResponceSize;
+      MessageMode::Type Mode;
+      ErrorCode::Type Error;
+    };
+
+  private:
+    bool HasError = false;
+    std::string Error;
+
+    bool MakeError(const std::string &msg);
+
+  public:
+    bool IsOk() const;
+    std::string GetError() const;
+
+    bool Send(SerialInterface &si, Request request);
+    bool Recieve(SerialInterface &si, Responce &responce, uint8_t responceSize);
   };
-
-  struct Request {
-    const uint8_t *Data;
-    uint8_t RequestSize;
-    uint8_t ResponceSize;
-    MessageMode::Type Mode;
-  };
-
-  struct Responce {
-    const uint8_t *Data;
-    uint8_t ResponceSize;
-    MessageMode::Type Mode;
-  };
-
-private:
-  bool HasError = false;
-  std::string Error;
-
-public:
-  bool IsOk() const;
-  std::string GetError() const;
-
-  bool Send(SerialInterface &si, Request request);
-  bool Recieve(SerialInterface &si, Responce &responce, uint8_t responceSize);
-};
 
 } // namespace Roki
--- src/BodyRPC.cpp
+++ src/BodyRPC.cpp
@@ -1,52 +1,77 @@
 #include "BodyRPC.hpp"
 
-namespace Roki {
+namespace Roki
+{
 
-auto BodyRPC::MessageMode::Serialize(MessageMode::Type mode) -> uint8_t {
-  return mode;
-}
+  auto BodyRPC::MessageMode::Serialize(MessageMode::Type mode) -> uint8_t
+  {
+    return mode;
+  }
 
-auto BodyRPC::MessageMode::Deserialize(uint8_t data) -> Type {
-  return data;
-}
+  auto BodyRPC::MessageMode::Deserialize(uint8_t data) -> Type
+  {
+    return data;
+  }
 
-bool BodyRPC::IsOk() const { return !HasError; }
-std::string BodyRPC::GetError() const { return Error; }
-
-bool BodyRPC::Send(SerialInterface &si, Request request) {
-  SerialInterface::OutPackage out{
-      SerialInterface::Periphery::Body, request.ResponceSize,
-      MessageMode::Serialize(request.Mode), request.Data, request.RequestSize};
-
-  if (!si.Send(out)) {
+  bool BodyRPC::MakeError(const std::string &msg)
+  {
     HasError = true;
-    Error = si.GetError();
+    Error = msg;
     return false;
   }
 
-  return true;
-}
+  auto BodyRPC::ErrorCode::Serialize(Type mode) -> uint8_t { return mode; }
+  auto BodyRPC::ErrorCode::Deserialize(uint8_t val) -> Type { return val; }
 
-bool BodyRPC::Recieve(SerialInterface &si, Responce &responce,
-                            uint8_t responceSize) {
-  SerialInterface::InPackage package;
-
-  if (!si.Receive(responceSize, package)) {
-    HasError = true;
-    Error = si.GetError();
-    return false;
+  std::string BodyRPC::ErrorCode::GetDescription(Type error)
+  {
+    switch (error)
+    {
+    case Success:
+      return "Success";
+    case Timeout:
+      return "Body timeout";
+    case Unknown:
+      return "Unknow body error";
+    default:
+      return "Unknown error value";
+    }
   }
 
-  if (package.Error != 0) {
-    HasError = true;
-    Error = "Body request failed";
+  bool BodyRPC::IsOk() const { return !HasError; }
+  std::string BodyRPC::GetError() const { return Error; }
+
+  bool BodyRPC::Send(SerialInterface &si, Request request)
+  {
+    SerialInterface::OutPackage out{
+        SerialInterface::Periphery::Body, request.ResponceSize,
+        MessageMode::Serialize(request.Mode), request.Data, request.RequestSize};
+
+    if (!si.Send(out))
+      return MakeError(si.GetError());
+
+    return true;
   }
 
-  responce.Data = package.Data;
-  responce.ResponceSize = package.ResponceSize;
-  responce.Mode = MessageMode::Deserialize(package.MetaInfo);
+  bool BodyRPC::Recieve(SerialInterface &si, Responce &responce,
+                        uint8_t responceSize)
+  {
+    SerialInterface::InPackage package;
 
-  return true;
-}
+    if (!si.Receive(responceSize, package))
+      return MakeError(si.GetError());
+
+    ErrorCode::Type error = ErrorCode::Deserialize(package.Error);
+
+    if (error != ErrorCode::Success)
+      return MakeError(ErrorCode::GetDescription(error));
+
+    responce.Data = package.Data;
+    responce.ResponceSize = package.ResponceSize;
+    responce.Error = error;
+    responce.Mode = MessageMode::Deserialize(package.MetaInfo);
+
+    return true;
+  }
 
 } // namespace Roki
