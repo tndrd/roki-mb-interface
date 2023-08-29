54d67b15eed19b6f2a9f3eea562c5a36da5160cf
Vladimir Lekhterev <vovalekhterev@gmail.com>
2023-08-29 15:47:46 +0300

    Rewrite rcb4 usage

---
+++ inc/RokiRcb4.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Motherboard.hpp"
+#include "Rcb4BaseClass.hpp"
+
+namespace Roki {
+
+class RokiRcb4 : public Rcb4BaseClass {
+private:
+  Motherboard *Mboard;
+
+  bool HasError = false;
+  std::string Error;
+
+private:
+  bool MakeError(const std::string &msg);
+
+public:
+  RokiRcb4(Motherboard &mb);
+  bool IsOk() const;
+  std::string GetError() const;
+
+private:
+  virtual bool synchronize(byte txBuf[], byte txLen, byte rxBuf[],
+                           byte rxLen) override;
+
+  bool SendAsync(byte *txData);
+
+public:
+  bool SetServoPosAsync(ServoData servoDatas[], byte servoCount, byte frame);
+};
+} // namespace Roki
---
+++ src/RokiRcb4.cpp
@@ -0,0 +1,43 @@
+#include "RokiRcb4.hpp"
+
+namespace Roki {
+
+bool RokiRcb4::MakeError(const std::string &msg) {
+  HasError = true;
+  Error = "Rcb4: " + msg;
+  return false;
+}
+
+RokiRcb4::RokiRcb4(Motherboard &mb) : Mboard(&mb) { assert(Mboard); }
+bool RokiRcb4::IsOk() const { return !HasError; }
+
+std::string RokiRcb4::GetError() const {
+  return HasError ? Error : "Internal rcb4 error";
+}
+
+bool RokiRcb4::synchronize(byte txBuf[], byte txLen, byte rxBuf[], byte rxLen) {
+  if (!Mboard->BodySendSync(txBuf, txLen, rxBuf, rxLen))
+    return MakeError("Failed to synchronize: " + Mboard->GetError());
+  
+  HasError = false;
+  return true;
+}
+
+bool RokiRcb4::SendAsync(byte *txData) {
+  uint8_t txSize = *txData;
+
+  if (!Mboard->BodySendAsync(txData, txSize, 4))
+    return MakeError("Failed to send async: " + Mboard->GetError());
+  
+  HasError = false;
+  return true;
+}
+
+bool RokiRcb4::SetServoPosAsync(ServoData servoDatas[], byte servoCount,
+                                byte frame) {
+  if (runConstFrameServoCmd(servoDatas, servoCount, frame) == -1)
+    return MakeError("Failed to set servo pos: ill command");
+  return SendAsync(txBuf);
+}
+
+} // namespace Roki
