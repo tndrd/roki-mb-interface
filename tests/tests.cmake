d509e7a763de08c784e313887895bb844c28c3c0
Vladimir Lekhterev <vovalekhterev@gmail.com>
2023-08-29 15:48:37 +0300

    Update due to previous changes

--- tests/tests.cmake
+++ tests/tests.cmake
@@ -2,7 +2,7 @@ add_executable(TestIMU tests/TestIMU.cpp)
 target_link_libraries(TestIMU PRIVATE Motherboard pthread)
 
 add_executable(TestBody tests/TestBody.cpp)
-target_link_libraries(TestBody PRIVATE Motherboard Rcb4Adapter pthread)
+target_link_libraries(TestBody PRIVATE Motherboard RokiRcb4 pthread)
 
 add_executable(TestAdapter tests/TestAdapter.cpp)
-target_link_libraries(TestAdapter PRIVATE MotherboardAdapter Rcb4Adapter pthread)
+target_link_libraries(TestAdapter PRIVATE MotherboardAdapter RokiRcb4 pthread)
