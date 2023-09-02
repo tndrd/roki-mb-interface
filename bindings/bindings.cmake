be1c73291bf1550972f2e44ed62a0b6179c82801
Vladimir Lekhterev <vovalekhterev@gmail.com>
2023-09-02 16:14:03 +0300

    Add bindings installation

--- bindings/bindings.cmake
+++ bindings/bindings.cmake
@@ -2,3 +2,11 @@ set(CMAKE_CXX_FLAGS "-fPIC")
 
 pybind11_add_module(Roki bindings/MBBinding.cpp)
 target_link_libraries(Roki PUBLIC MotherboardAdapter RokiRcb4Adapter)
+
+find_package(Python3 COMPONENTS Interpreter Development REQUIRED)
+
+install(TARGETS Roki
+        COMPONENT python
+        LIBRARY DESTINATION "lib/python${Python3_VERSION_MAJOR}.${Python3_VERSION_MINOR}/site-packages"
+        ARCHIVE DESTINATION "lib"
+        RUNTIME DESTINATION "bin")
--- tests/scripts/TestBody.py
+++ tests/scripts/TestBody.py
@@ -1,5 +1,4 @@
 import sys
-sys.path.append('/home/pi/Tndrd/roki-mb-interface/build')
 import time
 
 import Roki
