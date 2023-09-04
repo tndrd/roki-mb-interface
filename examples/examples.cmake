0b01f5dafb2c12fe266f3d1ae223b95304ad061e
spanovs01 <spanovs01frag@egmail.com>
2023-09-04 19:20:24 +0300

    Update cmakelists

--- examples/examples.cmake
+++ examples/examples.cmake
@@ -1,8 +1,8 @@
-add_executable(TestIMU tests/TestIMU.cpp)
+add_executable(TestIMU examples/TestIMU.cpp)
 target_link_libraries(TestIMU PRIVATE Motherboard pthread)
 
-add_executable(TestBody tests/TestBody.cpp)
+add_executable(TestBody examples/TestBody.cpp)
 target_link_libraries(TestBody PRIVATE Motherboard RokiRcb4 pthread)
 
-add_executable(TestAdapter tests/TestAdapter.cpp)
+add_executable(TestAdapter examples/TestAdapter.cpp)
 target_link_libraries(TestAdapter PRIVATE MotherboardAdapter RokiRcb4 pthread)
