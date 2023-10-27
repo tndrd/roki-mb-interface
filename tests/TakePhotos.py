dba272cebe6a72531f40cdbe19cd049165d1dd1d
spanovs01 <spanovs01frag@egmail.com>
2023-10-27 16:30:15 +0300

    Make TakePhotos accept time as argv[1]

--- tests/TakePhotos.py
+++ tests/TakePhotos.py
@@ -3,8 +3,12 @@ import RokiPyTest as rpt
 import Roki
 import cv2
 import time
+import sys
+rpt.mute_picamera()
 
-TEST_TIME_S = 1
+assert len(sys.argv) == 2
+
+TEST_TIME_S = int(sys.argv[1])
 FRAME_DURATION_US = 16700
 FRAME_DURATION_MS = FRAME_DURATION_US // 1000
 DURATION_THRESHOLD = 4
@@ -18,9 +22,10 @@ rpt.call(mb, mb.ResetStrobeContainers())
 rpt.call(mb, mb.ConfigureStrobeFilter(FRAME_DURATION_MS, DURATION_THRESHOLD))
 
 picam2.start()
-rpt.eprint(f"Running for {TEST_TIME_S}s...")
+rpt.eprint(f"Running for {TEST_TIME_S + 1}s...")
 time.sleep(1)
 picam2.set_controls({"FrameDurationLimits": (FRAME_DURATION_US, FRAME_DURATION_US)})
+time.sleep(TEST_TIME_S)
 picam2.stop()
 
 imu = rpt.call(mb, mb.GetIMUContainerInfo())
