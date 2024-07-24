91f95043b915549737b51df943a8e088bc2e43f1
Vladimir Lekhterev <vovalekhterev@gmail.com>
2024-07-24 17:54:46 +0300

    Fix typo

--- bindings/bindings.cmake
+++ bindings/bindings.cmake
@@ -5,6 +5,6 @@ target_link_libraries(Roki PUBLIC MotherboardAdapter RokiRcb4Adapter MbDefaultCo
 
 find_package(Python3 COMPONENTS Interpreter Development REQUIRED)
 
-message(INFO "Python site-packages directory: ${Python3_SITEARCH}")
+message(INFO " Python site-packages directory: ${Python3_SITEARCH}")
 
 install(TARGETS Roki DESTINATION ${Python3_SITEARCH})
