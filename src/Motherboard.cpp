1b35bca111ef5bd3728d7f95e90e9c76b327bc1d
Vladimir Lekhterev <vovalekhterev@gmail.com>
2025-07-03 15:48:18 +0300

    Remove synchronization logic

--- src/Motherboard.cpp
+++ src/Motherboard.cpp
@@ -35,7 +35,7 @@ bool CheckVersion(Version version) {
 
 #define FOO_ERROR(msg) MakeFooError(__func__, msg)
 #define CHECK_CLIENT_ERROR ok ? true : FOO_ERROR(Client.GetError())
-#define CREATE_GUARD std::lock_guard<std::mutex> _(Mutex)
+#define CREATE_GUARD //std::lock_guard<std::mutex> _(Mutex)
 
 bool Motherboard::Configure(const TTYConfig &config) {
   CREATE_GUARD;
